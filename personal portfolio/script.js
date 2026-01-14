703d53bd4d2d5dc216c08dd876881a0908c26cf1
Arya Waskar <aryawaskar1@gmail.com>
2026-01-14 12:36:28 +0530

    Initial portfolio

---
+++ personal portfolio/script.js
@@ -0,0 +1,58 @@
+const progressBars = document.querySelectorAll(".progress");
+
+window.addEventListener("scroll", () => {
+  const triggerPoint = window.innerHeight - 100;
+  
+  progressBars.forEach(bar => {
+    const top = bar.getBoundingClientRect().top;
+    if(top < triggerPoint){
+      bar.style.width = bar.getAttribute("data-width");
+    }
+  });
+});
+
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Thank you 💜 Your message has been sent!");
+  form.reset();
+});
+
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+const sections = document.querySelectorAll(".fade-section");
+
+const revealOnScroll = () => {
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const triggerPoint = window.innerHeight - 100;
+
+    if (sectionTop < triggerPoint) {
+      section.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+/* Close menu when clicking a link */
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
